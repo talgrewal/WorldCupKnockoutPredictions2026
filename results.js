8bce5d6612ae843a1dc145faeffc1b6d66cdd0ce
talgrewal <51344070+talgrewal@users.noreply.github.com>
2026-06-28 03:32:08 -0700

    Update results.js

--- results.js
+++ results.js
@@ -1,1 +1,192 @@
-
+{
+  "goldenBoot": "Billy Fam",
+  "goldenBall": "harry Billy",
+  "score": [
+    {
+      "game": "Game1",
+      "stage": "Round of 32",
+      "points": 1,
+      "score": "NotPlayedYet"
+    },
+    {
+      "game": "Game2",
+      "stage": "Round of 32",
+      "points": 1,
+      "score": "🇲🇦MoroccoW🇳🇱Netherlands"
+    },
+    {
+      "game": "Game3",
+      "stage": "Round of 32",
+      "points": 1,
+      "score": "🇫🇷FranceW🇸🇪Sweden"
+    },
+    {
+      "game": "Game4",
+      "stage": "Round of 32",
+      "points": 1,
+      "score": "🇵🇾ParaguayW🇩🇪Germany"
+    },
+    {
+      "game": "Game5",
+      "stage": "Round of 32",
+      "points": 1,
+      "score": "🇭🇷CroatiaW🇵🇹Portugal"
+    },
+    {
+      "game": "Game6",
+      "stage": "Round of 32",
+      "points": 1,
+      "score": "🇦🇹AustriaW🇪🇸Spain"
+    },
+    {
+      "game": "Game7",
+      "stage": "Round of 32",
+      "points": 1,
+      "score": "🇧🇦Bosnia-HerzegovinaW🇺🇸United States"
+    },
+    {
+      "game": "Game8",
+      "stage": "Round of 32",
+      "points": 1,
+      "score": "🇸🇳SenegalW🇧🇪Belgium"
+    },
+    {
+      "game": "Game9",
+      "stage": "Round of 32",
+      "points": 1,
+      "score": "🇯🇵JapanW🇧🇷Brazil"
+    },
+    {
+      "game": "Game10",
+      "stage": "Round of 32",
+      "points": 1,
+      "score": "🇨🇮Ivory CoastW🇳🇴Norway"
+    },
+    {
+      "game": "Game11",
+      "stage": "Round of 32",
+      "points": 1,
+      "score": "🇲🇽MexicoW🇪🇨Ecuador"
+    },
+    {
+      "game": "Game12",
+      "stage": "Round of 32",
+      "points": 1,
+      "score": "🇨🇩Congo DRW🇬🇧England"
+    },
+    {
+      "game": "Game13",
+      "stage": "Round of 32",
+      "points": 1,
+      "score": "🇨🇻Cape VerdeW🇦🇷Argentina"
+    },
+    {
+      "game": "Game14",
+      "stage": "Round of 32",
+      "points": 1,
+      "score": "🇪🇬EgyptW🇦🇺Australia"
+    },
+    {
+      "game": "Game15",
+      "stage": "Round of 32",
+      "points": 1,
+      "score": "NotPlayedYet"
+    },
+    {
+      "game": "Game16",
+      "stage": "Round of 32",
+      "points": 1,
+      "score": "NotPlayedYet"
+    },
+    {
+      "game": "Game17",
+      "stage": "Round of 16",
+      "points": 2,
+      "score": "🇲🇦MoroccoW🇨🇦Canada"
+    },
+    {
+      "game": "Game18",
+      "stage": "Round of 16",
+      "points": 2,
+      "score": "NotPlayedYet"
+    },
+    {
+      "game": "Game19",
+      "stage": "Round of 16",
+      "points": 2,
+      "score": "NotPlayedYet"
+    },
+    {
+      "game": "Game20",
+      "stage": "Round of 16",
+      "points": 2,
+      "score": "🇸🇳SenegalW🇧🇦Bosnia-Herzegovina"
+    },
+    {
+      "game": "Game21",
+      "stage": "Round of 16",
+      "points": 2,
+      "score": "🇨🇮Ivory CoastW🇯🇵Japan"
+    },
+    {
+      "game": "Game22",
+      "stage": "Round of 16",
+      "points": 2,
+      "score": "🇨🇩Congo DRW🇲🇽Mexico"
+    },
+    {
+      "game": "Game23",
+      "stage": "Round of 16",
+      "points": 2,
+      "score": "🇪🇬EgyptW🇨🇻Cape Verde"
+    },
+    {
+      "game": "Game24",
+      "stage": "Round of 16",
+      "points": 2,
+      "score": "🇨🇭SwitzerlandW🇨🇴Colombia"
+    },
+    {
+      "game": "Game25",
+      "stage": "Quarterfinals",
+      "points": 4,
+      "score": "🇵🇾ParaguayW🇲🇦Morocco"
+    },
+    {
+      "game": "Game26",
+      "stage": "Quarterfinals",
+      "points": 4,
+      "score": "🇸🇳SenegalW🇦🇹Austria"
+    },
+    {
+      "game": "Game27",
+      "stage": "Quarterfinals",
+      "points": 4,
+      "score": "🇨🇮Ivory CoastW🇨🇩Congo DR"
+    },
+    {
+      "game": "Game28",
+      "stage": "Quarterfinals",
+      "points": 4,
+      "score": "🇪🇬EgyptW🇨🇭Switzerland"
+    },
+    {
+      "game": "Game29",
+      "stage": "Semifinals",
+      "points": 8,
+      "score": "NotPlayedYet"
+    },
+    {
+      "game": "Game30",
+      "stage": "Semifinals",
+      "points": 8,
+      "score": "NotPlayedYet"
+    },
+    {
+      "game": "Game31",
+      "stage": "Final",
+      "points": 16,
+      "score": "NotPlayedYet"
+    }
+  ]
+}
